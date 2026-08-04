1c9dd21e9f9bf10cd75cb73b75ed3d637f293a6a
Odon Candido <odon@kali.odon>
2026-08-05 01:57:01 +0200

    Comentarios no .cpp e .h

--- src/GSMController.cpp
+++ src/GSMController.cpp
@@ -1,68 +1,69 @@
 #include "GSMController.h"
 
-GSMController::GSMController(HardwareSerial &serial) 
-  : sim800l(serial) {}
+/**
+ * @brief configura o modem
+ * @param void
+ * @return void
+ * */
+GSMController::GSMController(HardwareSerial &serial)  : sim800l(serial) {
+    sim800l.println("AT+CMGD=1,4"); 		//Apaga SMS da memória
+    sim800l.println("AT+CFUN=1");	 	//Coloca o módulo em funcionamento completo
+    sim800l.println("AT+CNMI=2,2,0,0,0"); 	//Configura como novos SMS serão entregues
+    sim800l.println("AT+CMGF=1")	 	//Coloca SMS no modo texto
+    sim800l.println("AT+CLIP=1")	 	//Ativa identificação do número de quem liga
+    sim800l.println("AT+DDET=1,0,0")	 	//Ativa detecção de tons DTMF
+
+}
+/**
+ * @brief Processa uma resposta recebida do modulo GSM
+ * @param response Reposta retornada pelo GSM
+ * @return Array contendo as informacoes processadas
+ * */
 String* GSMController::processGsmInformation(String response) {
-  static String result[4];  // Array para armazenar a informação processadaz
-  /*if (response.indexOf("+CMTI:") > -1) {
-    int startIndex = response.indexOf(",") + 1;
-    String indice = response.substring(startIndex);
-    indice.trim();  
-
-    Serial.println("Novo SMS detectado, lendo...");
-    Serial.println("Índice: " + indice);
-
-    String comando = "AT+CMGR=" + indice;
-    String respostaSMS = sendATCommand(comando);  // Obtém a resposta do AT+CMGR
-    String* smsInfo = processSMS(respostaSMS);  // Processa a mensagem e 
-    //String* smsInfo = processSMS(response);
-    result[0] = "SMS";          // Tipo de evento
-    result[1] = smsInfo[0];     // Número do remetente
-    result[2] = smsInfo[1];     // Data/Hora
-    result[3] = smsInfo[2];     // Corpo da mensagemretorna os dados
-  }*/
-
-  
+  static String result[4];  
   if (response.indexOf("+CMT:") > -1) {  
     String* smsInfo = processSMS(response);
-    result[0] = "SMS";          
+    result[0] = "SMS";         
     result[1] = smsInfo[0];     
     result[2] = smsInfo[1];     
     result[3] = smsInfo[2];     
-  } 
-  else if (response.indexOf("RING") > -1){
+  }else if (response.indexOf("RING") > -1){
     result[0] = "CALL";         
-    result[1] = "Telefone chamando";
-    result[2] = obterNumeroChamada();  //AT+CLCC
+    result[1] = "Phone ringing";
+    result[2] = obterNumeroChamada();
     result[3] = "";
-  } 
-  else if (response.indexOf("NO CARRIER") > -1) {
+  }else if (response.indexOf("NO CARRIER") > -1) {
     result[0] = "CALL_END";
-    result[1] = "Chamada perdida!";
+    result[1] = "Missed Call!";
     result[2] = "";
     result[3] = "";
     resetarChamada();
-  } 
-  else if(response.indexOf("BUSY") > -1){
+  }else if(response.indexOf("BUSY") > -1){
     result[0] = "BUSY";
     result[1] = "Linha ocupada!";
     result[2] = "";
     result[3] = "";
-  }
-  else if(response.indexOf("NO DIALTONE") > -1){
+  }else if(response.indexOf("NO DIALTONE") > -1){
     result[0] = "NO DIALTONE";
     result[1] = "Sem Linha";
     result[2] = "";
     result[3] = "";
-  }
-  else if(response.indexOf("NO ANSWER") > -1){
+  }else if(response.indexOf("NO ANSWER") > -1){
     result[0] = "NO ANSWER";
     result[1] = "Nao Atende";
     result[2] = "";
     result[3] = "";
-  }
-  
-  else {
+  }else if(response.indexOf("UNDER_VOLTAGE_WARNING") > -1){
+    result[0] = "UNDER_VOLTAGE_WARNING";
+    result[1] = "Tensão aciama do limite! > 4.2 V";
+    result[2] = "";
+    result[3] = "";
+  }else if(response.indexOf("LOW_VOLTAGE_WARINIG") > -1){
+    result[0] = "LOW VOLTAGE WARNING";
+    result[1] = "Tensão baixa!";
+    result[2] = "";
+    result[3] = "";
+  }else{
     result[0] = "OTHER";
     result[1] = response;
     result[2] = "";
@@ -72,10 +73,8 @@ String* GSMController::processGsmInformation(String response) {
   return result;
 }
 
-
 String* GSMController::processSMS(String response) {
   static String smsInfo[3];
-
   int numStart = response.indexOf("\"") + 1;
   int numEnd = response.indexOf("\"", numStart);
   smsInfo[0] = response.substring(numStart, numEnd);  
@@ -107,50 +106,15 @@ void GSMController::callNumber(String number) {
 void GSMController::hangUpCall() {
   sendATCommand("ATH");
 }
-/*
-String GSMController::obterNumeroChamada() {
-  if (!numCallReturned) {
-    String response = sendATCommand("AT+CLCC");
-
-    // Quebra a resposta em linhas (caso o modem envie múltiplas)
-    int clccIndex = response.indexOf("+CLCC:");
-    if (clccIndex != -1) {
-      // Isola a linha com +CLCC:
-      int lineEnd = response.indexOf("\n", clccIndex);
-      String clccLine = (lineEnd != -1) ? response.substring(clccIndex, lineEnd) : response.substring(clccIndex);
-
-      // Extrai o número entre aspas
-      int start = clccLine.indexOf("\"") + 1;
-      int end = clccLine.indexOf("\"", start);
-      
-      if (start > 0 && end > start) {
-        num = clccLine.substring(start, end);
-        numCallReturned = true;
-        return num;
-      } else {
-        return "Erro ao extrair número: " + clccLine;
-      }
-    } else {
-      return "Sem resposta CLCC: " + response;
-    }
-  }
-
-  return num; // Retorna o número salvo
-}
-*/
 
 String GSMController::obterNumeroChamada() {
-  //if (numCallReturned) {
-  //  return "";  // Evita repetir a consulta
-  //}
-
   if (!numCallReturned){
     String response = sendATCommand("AT+CLCC");
     int start = response.indexOf("\"") + 1;
     int end = response.indexOf("\"", start);
   
     if (start > 0 && end > start) {
-      numCallReturned = true;  // Ativa a flag para evitar repetição
+      numCallReturned = true;
       num = response.substring(start, end);  
       return num;
     } 
--- src/GSMController.h
+++ src/GSMController.h
@@ -1,21 +1,21 @@
 #ifndef GSM_CONTROLLER_H
 #define GSM_CONTROLLER_H
 #include <HardwareSerial.h>
-#if CONFIG_IDF_TARGET_ESP32        // ESP32 original
+#if CONFIG_IDF_TARGET_ESP32        // ESP32 WROOM 32
   #define SIM800L_TX_PIN 17
   #define SIM800L_RX_PIN 16
   #define LED_BULTIN     2
 #elif CONFIG_IDF_TARGET_ESP32C3    // ESP32-C3 (SuperMini, DevKit, etc)
   #define SIM800L_TX_PIN 0
   #define SIM800L_RX_PIN 1
- // #define LED_BUILTIN    8
+  #define LED_BUILTIN    8
 
 #elif CONFIG_IDF_TARGET_ESP32S3    // ESP32-S3
   #define SIM800L_TX_PIN 20
   #define SIM800L_RX_PIN 19
-
+  #define LED_BUILTIN    2
 #else
-  #error "Placa ESP32 não suportada!"
+  #error "Dev Board uknown!"
 #endif
 #define TINY_GSM_MODEM_SIM800
 #include <TinyGsmClient.h>
@@ -31,7 +31,6 @@ class GSMController {
     String obterNumeroChamada();
     String num="";
     bool numCallReturned = false;
-    //String lerResposta();
     void callNumber(String number);
     void hangUpCall();
     void resetarChamada();
